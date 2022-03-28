4b34a26a63876c269226192fe5b4d7ce9d5df07e
Florian Dupeyron <florian.dupeyron@elsys-design.com>
2022-03-29 01:07:58 +0200

    Build docker image if needed

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ PWD=`pwd`
 mkdir -p output
 mkdir -p build
 
+[ -z "`docker image ls | grep docker-stm32`" ] && docker build -t docker-stm32 .
+
 docker run --rm -it \
 	--mount type=bind,src=${PWD}/scripts,dst=/scripts \
 	--mount type=bind,src=${PWD}/project,dst=/project \
